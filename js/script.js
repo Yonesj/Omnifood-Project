67978ffea46010665732c53025d18cbf920417d7
Younes Jamshidi <jamshidiyounes92@gmail.com>
2024-07-19 16:21:58 +0330

    add functionality to navigation btn

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// set current year
+const year = document.querySelector(".year");
+year.textContent = new Date().getFullYear();
+
+// add functionality to mobile nav
+const navBtn = document.querySelector(".mobile-nav");
+const headerEl = document.querySelector(".header");
+
+navBtn.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
